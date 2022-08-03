d7d02c479848c94dc69e3037d175a8a10fcb0b89
mirzaarslan <mirzaarslan450@gmail.com>
2022-08-04 02:52:59 +0400

    implementing RetrofitRequestExecutor.kt increase timeout for network call

---
+++ common/network/src/main/java/com/arslan/network/NetworkResponse.kt
@@ -0,0 +1,9 @@
+package com.arslan.network
+
+import com.google.gson.JsonElement
+
+sealed class NetworkResponse {
+    data class Success(val response: JsonElement?, val statusCode: Int): NetworkResponse()
+    data class Failure(val response: JsonElement?, val statusCode: Int): NetworkResponse()
+    object Loading: NetworkResponse()
+}
