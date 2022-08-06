fc7c124173266961e5aadfe6d6eda73a02636021
mirzaarslan <mirzaarslan450@gmail.com>
2022-08-06 18:21:00 +0400

    working in request builder

--- common/network/src/main/java/com/arslan/network/model/ServerResponseModel.kt
+++ common/network/src/main/java/com/arslan/network/model/ServerResponseModel.kt
@@ -2,4 +2,4 @@ package com.arslan.network.model
 
 import com.google.gson.JsonElement
 
-class ServerResponseModel(var status: Int = -1, var model: JsonElement? = null)
+data class ServerResponseModel(var status: Int = -1, var model: JsonElement? = null)
