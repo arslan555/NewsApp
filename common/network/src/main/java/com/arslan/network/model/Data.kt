1b3c8bc8a32fff90b4eb18f46dc024532914082b
Mirza Arslan <arslan555@users.noreply.github.com>
2022-08-20 19:25:23 +0400

    refactoring network models

--- common/network/src/main/java/com/arslan/network/model/Data.kt
+++
@@ -1,3 +0,0 @@
-package com.arslan.network.model
-
-data class Data(val response: String)
--- common/network/src/main/java/com/arslan/network/model/ServerResponseModel.kt
+++
@@ -1,5 +0,0 @@
-package com.arslan.network.model
-
-import com.google.gson.JsonElement
-
-data class ServerResponseModel(var status: Int = -1, var model: JsonElement? = null)
