da838073a7f31bfe7653b36765ca9f55f558521e
mirzaarslan <mirzaarslan450@gmail.com>
2022-08-25 00:31:54 +0400

    implement NewsSource API

--- app/src/main/java/com/arslan/nytimesnewsapp/NewsRepo.kt
+++ app/src/main/java/com/arslan/nytimesnewsapp/NewsRepo.kt
@@ -5,6 +5,7 @@ import com.arslan.network.model.ApiError
 import com.arslan.network.model.ApiException
 import com.arslan.network.model.ApiSuccess
 import com.arslan.network.request.RequestBuilder
+import com.arslan.nytimesnewsapp.model.NewsSources
 
 import com.google.gson.reflect.TypeToken
 import timber.log.Timber
@@ -13,14 +14,14 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class NewsRepo @Inject constructor(private val requestExecutor: RequestExecutor) {
-    private val userRequest  = RequestBuilder.createBasicGetRequest("/users",
-        object: TypeToken<UserResponse?>() {}.type)
+    private val userRequest  = RequestBuilder.createBasicGetRequest("top-headlines/sources",
+        object: TypeToken<NewsSources?>() {}.type)
 
     suspend operator fun invoke() {
-        when(val response =  requestExecutor.execute<UserResponse>(userRequest) ) {
+        when(val response =  requestExecutor.execute<NewsSources>(userRequest) ) {
             is ApiSuccess -> Timber.d("${response.data}")
-            is ApiError -> Timber.d("${response.message}")
-            is ApiException -> Timber.d("${response.e.message}")
+            is ApiError -> Timber.d(response.errorResponse.toString())
+            is ApiException -> Timber.e("${response.e.message}")
         }
     }
 
